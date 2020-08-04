c1b1dd8483b3b07289215fe03e612eef163f500d
hortencia718 <hortenciaecisneros@gmail.com>
2020-08-04 16:56:57 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,31 @@
+#  require 'pry'
+
+class Person 
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account 
+# binding.pry
+
+def initialize(name_args)
+    @name = name_args
+    @bank_account = 25
+    @happiness = 8
+    @hygiene =8
+    
+end
+ 
+
+ def happiness=(happy_arg)
+    # this is a writer method
+@happiness = happy_arg
+# this is where i am stuck!
+
+
+
+
+
+
+
+
+end
 # your code goes here
+# binding.pry 
